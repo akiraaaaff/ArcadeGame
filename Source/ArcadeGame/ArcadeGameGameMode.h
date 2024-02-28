af8c6c1e423b3ebc8a78afadf970614c22fa2dc8
Noardewyn <karpovichalexej@gmail.com>
2024-02-28 14:12:10 +0100

    Initial commit

---
+++ Source/ArcadeGame/ArcadeGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArcadeGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AArcadeGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AArcadeGameGameMode();
+};
+
+
+
---
+++ Source/ArcadeGameEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ArcadeGameEditorTarget : TargetRules
+{
+	public ArcadeGameEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		ExtraModuleNames.Add("ArcadeGame");
+	}
+}
